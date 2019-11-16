b47154d28b3da343cefdfb3c70aa9da1c8ba081b
Julia Gula <jgula@office.cdv.pl>
2019-11-16 12:17:40 +0100

    Cleaned the code

--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -6,7 +6,7 @@ namespace GameOfLife
     {
         static void Main(string[] args)
         {
-            var game = new Game(10, 10);
+            var game = new Game(20, 20);
             
             while(true)
             {
